279af122347ff790b51a44f040b6d95405b1e20a
Pranav 'Baba' Minasandra <pminasandra@ab.mpg.de>
2023-07-19 19:09:11 +0200

    Updated README, added pre-print, better calling p2

--- README.md
+++ README.md
@@ -1,7 +1,7 @@
 # hyena-behaviour-classifier-inator
 This repo contains code for predicting hyena behaviour from accelerometer data.
 The raw data used here was obtained as part of the [CCAS](https://movecall.group)
-project. This project is described in detail in our [upcoming pre-print].
+project. This project is described in detail in our [pre-print](https://www.biorxiv.org/content/10.1101/2023.05.31.543053v1.article-metrics).
 The members who contributed to the code in this repo are:
 - Pranav Minasandra
 - Ariana Strandburg-Peshkin
@@ -12,26 +12,6 @@ to fieldwork, data preprocessing, etc. include:
 - Andrew Gersick
 - Kay Holekamp
 - Eli Strauss
-
-# Licensing
-Copyright © 2020 Pranav Minasandra
-
-Permission is hereby granted, free of charge, to any person obtaining a copy of
-this software and associated documentation files (the "Software"), to deal in
-the Software without restriction, including without limitation the rights to
-use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of
-the Software, and to permit persons to whom the Software is furnished to do so,
-subject to the following conditions:
-
-The above copyright notice and this permission notice shall be included in all
-copies or substantial portions of the Software.
-
-THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS
-FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR
-COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER
-IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
-CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 
 
 # Usage
--- p2_vedba_classes.py
+++ p2_vedba_classes.py
@@ -9,110 +9,6 @@ DataDir = PROJECTROOT + DATA + "FeaturesInTotal/"
 
 Files = [f for f in os.listdir(DataDir) if f[-3:] == "csv"]
 
-#logvedbas = []
-#
-#for f in Files:
-#        firstLine = True
-#        print(f)
-#        with open(DataDir+f) as F:
-#                for line in F:
-#                        if firstLine:
-#                                firstLine = False
-#                                continue
-#                        logvedbas.append(log(float(line.split(",")[14])))
-#        print("Now we have", len(logvedbas), "reads.")
-#
-#plt.xlabel("log mean VeDBA")
-#plt.ylabel("Frequency")
-##plt.axvline(x=-3.4, color="black", linestyle="dotted")
-##plt.axvline(x=0,color="black", linestyle="dotted")
-#plt.axvspan(-9, -3.4, facecolor='blue', alpha=0.3)
-#plt.axvspan(-3.4, 0, facecolor='yellow', alpha=0.3)
-#plt.axvspan(0, 2, facecolor='red', alpha=0.3)
-#plt.xlim(-8.5, 1.5)
-#plt.hist(logvedbas, 100, density=True, color="black")
-#plt.savefig(PROJECTROOT + FIGURES + "VedbaDist.pgf")
-#plt.savefig(PROJECTROOT + FIGURES + "VedbaDist.png")
-
-## I found -3.4 and 0.0 to be suitable boundaries to draw thresholds from the histogram
-#
-#Dur = 0
-#CurrState = "Unknown"
-#LOWs = []
-#MEDs = []
-#HIGHs = []
-#
-#for f in Files:
-#        print(f)
-#        firstLine = True
-#        CurrState = "Unknown"
-#
-#        with open(DataDir+f) as F:
-#                for line in F:
-#                        if firstLine:
-#                                firstLine = False
-#                                continue
-#
-#                        logv = log(float(line.split(",")[14]))
-#
-#                        if logv < -3.4:
-#                                state = "Low"
-#                        elif logv < 0 and logv > -3.4:
-#                                state = "Med"
-#                        else:
-#                                state = "High"
-#
-#                        if state == CurrState:
-#                                Dur += WINDOW_DURATION
-#
-#                        else:
-#                                if CurrState == "Low":
-#                                        LOWs.append(Dur)
-#                                elif CurrState == "Med":
-#                                        MEDs.append(Dur)
-#                                elif CurrState == "High":
-#                                        HIGHs.append(Dur)
-#
-#                                Dur = 0
-#                                CurrState = state
-#        print("Now we have", len(LOWs), len(MEDs), len(HIGHs), "reads.")
-#
-#fig, axs = plt.subplots(1,3, sharex=True, sharey=True)
-#
-#fit = powerlaw.Fit(LOWs, xmin=3.0)
-#fit.plot_ccdf(color="black", linewidth=1.5, ax = axs[0])
-#fit.exponential.plot_ccdf(color='black', linestyle='dotted', label="Exponential", ax=axs[0])	#	Uncomment to show the exponential distribution on the plot
-#fit.power_law.plot_ccdf(color='green', linestyle='dotted', label="Zeta", ax=axs[0])
-#fit.lognormal.plot_ccdf(color='brown', linestyle='dotted', label="Lognormal", ax=axs[0])
-#fit.truncated_power_law.plot_ccdf(color='red', linestyle='dotted', label="Polylog", ax=axs[0])
-#axs[0].legend()
-#
-#fit = powerlaw.Fit(MEDs, xmin=3.0)
-#fit.plot_ccdf(color="black", linewidth=1.5, ax = axs[1])
-#fit.exponential.plot_ccdf(color='black', linestyle='dotted', label="Exponential", ax=axs[1])	#	Uncomment to show the exponential distribution on the plot
-#fit.power_law.plot_ccdf(color='green', linestyle='dotted', label="Zeta", ax=axs[1])
-#fit.lognormal.plot_ccdf(color='brown', linestyle='dotted', label="Lognormal", ax=axs[1])
-#fit.truncated_power_law.plot_ccdf(color='red', linestyle='dotted', label="Polylog", ax=axs[1])
-#
-#fit = powerlaw.Fit(HIGHs, xmin=3.0)
-#fit.plot_ccdf(color="black", linewidth=1.5, ax = axs[2])
-#fit.exponential.plot_ccdf(color='black', linestyle='dotted', label="Exponential", ax=axs[2])	#	Uncomment to show the exponential distribution on the plot
-#fit.power_law.plot_ccdf(color='green', linestyle='dotted', label="Zeta", ax=axs[2])
-#fit.lognormal.plot_ccdf(color='brown', linestyle='dotted', label="Lognormal", ax=axs[2])
-#fit.truncated_power_law.plot_ccdf(color='red', linestyle='dotted', label="Polylog", ax=axs[2])
-#axs[0].set_ylabel('Complementary Cumulative Distribution Function')
-#axs[1].set_xlabel('Duration of Bout in Seconds')
-#axs[0].set_title('Low')
-#axs[1].set_title('Medium')
-#axs[2].set_title('High')
-#
-#axs[0].set_facecolor("#b2b2ff")
-#axs[1].set_facecolor("#ffffb2")
-#axs[2].set_facecolor("#ffb2b2")
-#
-#plt.savefig('VeDBA_bdds.png')
-#plt.savefig('VeDBA_bdds.pgf')
-#
 def hl(mlv):
         if mlv > -3.4:
                 return "HM"
@@ -168,4 +64,6 @@ def time_vs_activity_levels():
         plt.legend()
         plt.savefig(PROJECTROOT + FIGURES + "ActivityPatterns_VeDBA.pgf")
         plt.savefig(PROJECTROOT + FIGURES + "ActivityPatterns_VeDBA.png")
-time_vs_activity_levels()
+
+if __name__ == "__main__":
+    time_vs_activity_levels()
